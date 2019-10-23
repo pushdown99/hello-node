893b132d678989f2a39e5dac0b56198dc488be85
pushdown99 <haeyuen@gmail.com>
2019-10-23 15:22:02 +0900

    make it better

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var path    = require('path');
 var app     = express();
 var server  = require('http').createServer(app);
 var port    = process.env.PORT || 3000;
@@ -7,3 +8,15 @@ var port    = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log('Server listening at port %d', port);
 });
+
+app.use(express.urlencoded());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+app.get('/', function(req, res){
+  res.render('index');
+})
+
