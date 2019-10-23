2b33defe28354c1c86459e126f0b2827680b67cc
pushdown99 <haeyuen@gmail.com>
2019-10-23 15:42:10 +0900

    make it better

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path    = require('path');
 var app     = express();
+var cors    = require('cors');
 var server  = require('http').createServer(app);
 var port    = process.env.PORT || 3000;
 
